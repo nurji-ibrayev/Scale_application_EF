782351cc5340641522d37aa17a83d023173b7378
Nurji Ibrayev <30404638+nurji-ibrayev@users.noreply.github.com>
2023-05-23 01:47:23 +0600

    Add files via upload

---
+++ Scale_application/Scale_application/Models/Entities/Category.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Scale_application.Models.Entities
+{
+
+    public class Category
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<AdditionalField>? AdditionalFields { get; set; }
+    }
+
+    public class AdditionalField
+    {
+        public Guid AdditionalFieldId { get; set; }
+        public string AdditionalFieldName { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Guid CategoryId { get; set; }
+    }
+}
---
+++ Scale_application/Scale_application/Models/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Scale_application.Models.Entities
+{
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+        public string CategoryName { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+
+        [Required]
+        public string Price { get; set; }
+
+        public List<AdditionalFieldValue> AdditionalFields { get; set; }
+    }
+
+    public class AdditionalFieldValue
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Guid ProductId { get; set; }
+    }
+}
