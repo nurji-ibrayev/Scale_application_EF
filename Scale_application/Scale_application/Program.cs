67a8138a3cf9a74452ed7b19709ffe7a7528baa1
Nurji Ibrayev <30404638+nurji-ibrayev@users.noreply.github.com>
2023-05-23 01:49:23 +0600

    Add files via upload

---
+++ Scale_application/Scale_application/Program.cs
@@ -0,0 +1,33 @@
+using Scale_application.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ScaleDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CategoriesDbConnectionString")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
